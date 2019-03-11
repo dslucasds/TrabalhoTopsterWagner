508db1d24c5e1cb9e82ed8eefa4358eeaa0cff38
Aluno <Aluno@SN-279229>
2019-03-11 20:39:07 -0300

    Classe br.SisAgenda.modelo -Tarefa
    PainelCadasAgenda

---
+++ src/br/SisAgenda/modelo/Tarefa.java
@@ -0,0 +1,44 @@
+
+package br.SisAgenda.modelo;
+
+
+public class Tarefa {
+    
+    private String titAge;
+    private String desAge;
+    private long dataCri;
+    private long dataEnt;
+
+    public String getTitAge() {
+        return titAge;
+    }
+
+    public void setTitAge(String titAge) {
+        this.titAge = titAge;
+    }
+
+    public String getDesAge() {
+        return desAge;
+    }
+
+    public void setDesAge(String desAge) {
+        this.desAge = desAge;
+    }
+
+    public long getDataCri() {
+        return dataCri;
+    }
+
+    public void setDataCri(long dataCri) {
+        this.dataCri = dataCri;
+    }
+
+    public long getDataEnt() {
+        return dataEnt;
+    }
+
+    public void setDataEnt(long dataEnt) {
+        this.dataEnt = dataEnt;
+    }
+    
+}
